c6eb2a8c572bcb6473781e5aa9a8be0dc551d9bc
bash (curl -sL https://raw.githubusercontent.com/ga-instructors/installfest_script/pluto/builds/mac) <calyeung92@gmail.com>
2015-10-25 22:55:30 -0400

    added capture feature

--- js/server.js
+++ js/server.js
@@ -3,10 +3,12 @@ var game = new Phaser.Game(1200, 700, Phaser.AUTO, 'game', { preload: preload, c
 
 function preload() {
 	game.load.spritesheet('pokeball', "./assets/pokeball.png");
+	game.load.spritesheet('pikachu', "./assets/pikachu.png");
 	game.load.image('background', "./assets/background.jpg")
 
 }
-
+	var pokeball;
+	var pikachu;
 function create() {
 
 	background = game.add.sprite(0, 0, 'background')
@@ -14,11 +16,12 @@ function create() {
 
 
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-
+	pokeball = game.add.group()
+    pokeball.enableBody = true;
 	pokeball = game.add.sprite(10,10, "pokeball");
 	game.physics.enable(pokeball, Phaser.Physics.ARCADE);
 
-	pokeball.body.velocity.setTo(2800, 1100);
+	pokeball.body.velocity.setTo(1800, 500);
     
     pokeball.body.collideWorldBounds = true;
     
@@ -26,10 +29,26 @@ function create() {
 
     pokeball.scale.setTo(.3, .3)
 
-   	
+    
+    pikachu = game.add.sprite(400,300, 'pikachu');
+    game.physics.enable(pikachu, Phaser.Physics.ARCADE);
+    pikachu.body.collideWorldBounds = true;
+
+    pikachu.anchor.setTo(0.5,0.5)
+    pikachu.scale.setTo(1.5,1.5)
+    game.physics.arcade.enable(pikachu);
+    pikachu.body.allowRotation = true;
 }	
 
 function update() {
+	pikachu.rotation = game.physics.arcade.moveToPointer(pikachu, 60, game.input.activePointer, 500);
+	game.physics.arcade.overlap(pokeball, pikachu, touchPikachu, null, this);
+
+}
+
+
+function touchPikachu(pokeball, pikachu){
+	pikachu.kill()
 }
 
 function render () {
